b18bd6b15633ff104afbc14a8c70668971e57d08
pyoung <pyoungx7@gmail.com>
2018-03-17 19:42:57 -0400

    const variables like final in Java sanity check

---
+++ src/fundamentals/constVariables.js
@@ -0,0 +1,10 @@
+// can const variables be mutated?
+
+const arr = [];
+arr.push("HI");
+
+// yes.. the just cannot be reassigned just like final variables in Java
+console.log(arr);
+
+// illegal
+// arr = [];
